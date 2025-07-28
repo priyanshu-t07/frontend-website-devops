df2a2c109a41a645307e231571895d2e260d1d76
priyanshu-t07 <priyanshutripathy440@gmail.com>
2025-07-28 21:01:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/YOUR-REPO-NAME/',
+  base: '/frontend-website-devops/',
   build: {
     outDir: 'dist'
   }
